3f72589fbbe9d2b313870bb1619cf708f496bea6
Andrew Yuan <Andrew.Yuan@temporal.io>
2025-09-08 23:13:27 -0700

    clean up

--- core/src/worker/heartbeat.rs
+++ core/src/worker/heartbeat.rs
@@ -31,11 +31,6 @@ impl SharedNamespaceWorker {
         heartbeat_interval: Duration,
         telemetry: Option<WorkerTelemetry>,
     ) -> Self {
-        println!(
-            "SharedNamespaceWorker::new() {:?}\n\tsdk_name_and_version key{:?}",
-            client.get_identity(),
-            client.sdk_name_and_version()
-        );
         let config = WorkerConfigBuilder::default()
             .namespace(namespace.clone())
             .task_queue(format!(
@@ -176,7 +171,6 @@ impl fmt::Debug for SharedNamespaceWorker {
 }
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::{
         test_help::{WorkerExt, test_worker_cfg},
         worker,
